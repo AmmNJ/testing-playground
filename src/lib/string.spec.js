bacc939b28087ec9c455e68e0437a81246544b30
Ammersken Nils <nilsammersken@gmail.com>
2021-02-25 22:48:06 +0100

    Pass all test except for title case

--- src/lib/string.spec.js
+++ src/lib/string.spec.js
@@ -4,17 +4,17 @@ describe('lib/string', () => {
   describe('commaSeparatedToArray', () => {
     it('returns ["a", "b"] for text "a,b"', () => {
       const result = commaSeparatedToArray('a,b')
-      expect(result).toStrictEqual(['A', 'B'])
+      expect(result).toStrictEqual(['a', 'b'])
     })
 
     it('returns ["a", "b"] for text "a , b   "', () => {
       const result = commaSeparatedToArray('a , b   ')
-      expect(result).toStrictEqual(['A', 'B'])
+      expect(result).toStrictEqual(['a', 'b'])
     })
 
     it('returns ["a b", "c"] for text "a b, c"', () => {
       const result = commaSeparatedToArray('a b, c')
-      expect(result).toStrictEqual(['A B', 'C'])
+      expect(result).toStrictEqual(['a b', 'c'])
     })
 
     it('returns ["Jane", "John", "Joe"] for text "Jane, John,Joe"', () => {
@@ -25,12 +25,12 @@ describe('lib/string', () => {
     // Array in alphabetical order (sort)
     it('returns the array in ascending alphabetical order', () => {
       const result = commaSeparatedToArray('b, a, c')
-      expect(result).toStrictEqual(['A', 'B', 'C'])
+      expect(result).toStrictEqual(['a', 'b', 'c'])
     })
     // filter array for duplicates
     it('contains each string only once in the resulting array', () => {
       const result = commaSeparatedToArray('b, a, b, c')
-      expect(result).toStrictEqual(['A', 'B', 'C'])
+      expect(result).toStrictEqual(['a', 'b', 'c'])
     })
     // modify all toLowerCase() and add the first letter to a capital letter
     it.todo('returns all items in title case ("john" -> "John")')
